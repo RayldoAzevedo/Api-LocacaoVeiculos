b2ea251db6da36119e1fdd12257ff7a3c165c895
Azevedo Rayldo <azevedorayldo@gmail.com>
2022-05-07 14:12:28 -0300

    heroku e postegres adicionado

--- src/main/java/com/projeto/web/expoveiculos/persistence/VeiculoRepository.java
+++ src/main/java/com/projeto/web/expoveiculos/persistence/VeiculoRepository.java
@@ -2,4 +2,6 @@ package com.projeto.web.expoveiculos.persistence;
 import org.springframework.data.repository.CrudRepository;
 import com.projeto.web.expoveiculos.model.*;
 
+
+
 public interface VeiculoRepository extends CrudRepository<Veiculo, Integer>{}
